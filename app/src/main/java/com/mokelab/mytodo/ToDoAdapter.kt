2f5eb21a545a56741e68898aa6f907d3e7a02900
Hirokazu Fukami <fkm@mokelab.com>
2021-04-02 22:25:01 +0900

    Step 14

---
+++ app/src/main/java/com/mokelab/mytodo/ToDoAdapter.kt
@@ -0,0 +1,55 @@
+package com.mokelab.mytodo
+
+import android.text.format.DateFormat
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.mokelab.mytodo.databinding.TodoItemBinding
+import com.mokelab.mytodo.model.todo.ToDo
+
+class ToDoAdapter: ListAdapter<ToDo, ToDoAdapter.ViewHolder>(
+    callbacks
+) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = TodoItemBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val todo = getItem(position)
+        holder.bindTo(todo)
+    }
+
+    class ViewHolder(
+        private val binding: TodoItemBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+        fun bindTo(todo: ToDo) {
+            binding.titleText.text = todo.title
+            binding.createdText.text = DateFormat.format(
+                "yyyy-MM-dd hh:mm:ss",
+                todo.created
+            )
+        }
+
+    }
+
+    companion object {
+        private val callbacks = object: DiffUtil.ItemCallback<ToDo>() {
+            override fun areItemsTheSame(oldItem: ToDo, newItem: ToDo): Boolean {
+                return oldItem._id == newItem._id
+            }
+
+            override fun areContentsTheSame(oldItem: ToDo, newItem: ToDo): Boolean {
+                return oldItem.title == newItem.title &&
+                        oldItem.created == newItem.created
+            }
+
+        }
+    }
+
+
+}
