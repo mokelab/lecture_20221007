4fc1fbb724a5044309550beb81940c9e94308575
Hirokazu Fukami <fkm@mokelab.com>
2022-10-05 21:52:11 +0900

    Use compose and add first composable

--- app/src/main/java/com/mokelab/mytodo/MainActivity.kt
+++ app/src/main/java/com/mokelab/mytodo/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.mokelab.mytodo
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.mokelab.mytodo.ui.theme.MyToDoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            MyToDoTheme {
+                MyToDoApp()
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/mokelab/mytodo/MyToDoApp.kt
@@ -0,0 +1,8 @@
+package com.mokelab.mytodo
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MyToDoApp() {
+
+}
