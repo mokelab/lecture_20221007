f7cef94c2cd812f0859ec3a0628974f6c3079069
Hirokazu Fukami <fkm@mokelab.com>
2022-10-05 22:05:16 +0900

    Add MainScreen

---
+++ app/src/main/java/com/mokelab/mytodo/MainScreen.kt
@@ -0,0 +1,47 @@
+package com.mokelab.mytodo
+
+import android.text.format.DateFormat
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(viewModel: MainViewModel, toCreate: () -> Unit) {
+    val list by viewModel.todoList.observeAsState(emptyList())
+
+    Scaffold(topBar = {},
+        floatingActionButton = {
+            FloatingActionButton(onClick = toCreate) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = "Add")
+            }
+        }
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            items(list) {
+                ListItem(
+                    headlineText = {
+                        Text(it.title)
+                    },
+                    overlineText = {
+                        Text(
+                            DateFormat.format(
+                                "yyyy-MM-dd hh:mm:ss",
+                                it.created
+                            ).toString()
+                        )
+                    }
+                )
+            }
+        }
+    }
+}
--- app/src/main/java/com/mokelab/mytodo/MyToDoApp.kt
+++ app/src/main/java/com/mokelab/mytodo/MyToDoApp.kt
@@ -1,8 +1,21 @@
 package com.mokelab.mytodo
 
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun MyToDoApp() {
+    val navController = rememberNavController()
 
+    NavHost(navController = navController, startDestination = "top") {
+        composable("top") {
+            val vm: MainViewModel = hiltViewModel()
+            MainScreen(vm, toCreate = {
+                navController.navigate("create")
+            })
+        }
+    }
 }
